be90e667cc6045d08f2ddc7d7f3ad46925ea25a7
Vladimir Ladygin <ladygin@lenvoku.ru>
2022-03-19 22:45:03 +0300

    Added balls

--- Basketball/ViewController.swift
+++ Basketball/ViewController.swift
@@ -9,8 +9,17 @@
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
+    
     @IBOutlet var sceneView: ARSCNView!
+    
+    let configuration = ARWorldTrackingConfiguration()
+    
+    var isHoopAdded = false {
+        didSet {
+            configuration.planeDetection = []
+            sceneView.session.run(configuration, options: .removeExistingAnchors)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,12 +34,11 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
+        
         
         //Detect vertical planes
         configuration.planeDetection = .vertical
-
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -43,16 +51,29 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
     // MARK - Methods
     
+    func getBall() -> SCNNode? {
+        let ball = SCNSphere(radius: 0.125)
+        ball.firstMaterial?.diffuse.contents = UIImage(named:"basketball")
+        
+        let ballNode = SCNNode(geometry: ball)
+        
+        guard let frame = sceneView.session.currentFrame else { return nil }
+        
+        ballNode.simdTransform = frame.camera.transform
+        
+        return ballNode
+    }
+    
     func getHoopNode() ->SCNNode {
         let scene = SCNScene(named: "Hoop.scn", inDirectory: "art.scnassets")!
         
         let hoopNode = scene.rootNode.clone()
         
-        hoopNode.eulerAngles.x -= .pi / 2
+        //        hoopNode.eulerAngles.x -= .pi / 2
         
         return hoopNode
     }
-
+    
     func getPlane(for anchor: ARPlaneAnchor) -> SCNNode {
         let extent = anchor.extent
         let plane = SCNPlane(width: CGFloat(extent.x), height: CGFloat(extent.z))
@@ -75,15 +96,15 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         plane.height = CGFloat(extent.z)
         plane.width = CGFloat(extent.x)
     }
-   
     
     
-     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         guard let planeAnchor = anchor as? ARPlaneAnchor, planeAnchor.alignment == .vertical else {
             return
         }
-      
-         node.addChildNode(getPlane(for: planeAnchor))
+        
+        node.addChildNode(getPlane(for: planeAnchor))
         
     }
     func renderer( _ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
@@ -93,14 +114,40 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         updatePlaneNode(node, for: planeAnchor)
     }
-}
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
     
-   
+    
+    @IBAction func userTapped(_ sender: UITapGestureRecognizer) {
+        if isHoopAdded {
+            // Add basketballs
+            guard let ballNode = getBall() else { return }
+            
+            sceneView.scene.rootNode.addChildNode(ballNode)
+            
+        } else {
+            let location = sender.location(in: sceneView )
+            
+            guard let result = sceneView.hitTest(location, types: .existingPlaneUsingExtent).first else { return }
+            
+            guard let anchor = result.anchor as? ARPlaneAnchor, anchor.alignment == .vertical else { return }
+            
+            // Get hope node and set its coordinates tp the point of user touch
+            let hoopNode = getHoopNode()
+            hoopNode.simdTransform = result.worldTransform
+            hoopNode.eulerAngles.x -= .pi / 2
+            
+            isHoopAdded = true
+            sceneView.scene.rootNode.addChildNode(hoopNode)
+            
+        }
+        
+    }
+}
+
+
+//    // Override to create and configure nodes for anchors added to the view's session.
+//    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+//        let node = SCNNode()
+//        return node
+//    }
+
+
